70ad471704f43e55f7d5df66c6f82acf1a9a51f0
InrcedibleHolg <programming18@verdunkelbar.de>
2020-04-29 08:35:15 +0200

    changable delay

--- bin/runjupiter.sh
+++ bin/runjupiter.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 docker run -p 8888:8888  --rm -d -v jupitervolume:/home/jupyter --name jupyter_server jupyter-wrk  jupyter notebook
 # wait for the server to start
-sleep 3
+waittime=10
+echo "Wait $waittime seconds for the server to start"
+sleep $waittime
 # get the token
 docker logs jupyter_server
